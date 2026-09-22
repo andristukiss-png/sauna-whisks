f6a56bffa73651faeb388a86cd8690d9165e713c
andristukiss-png <andristukiss@gmail.com>
2026-09-23 00:01:29 +0100

    Repair static route newline encoding

--- app/journal/[slug]/page.tsx
+++ app/journal/[slug]/page.tsx
@@ -8,7 +8,9 @@ import { Breadcrumbs } from "@/components/Breadcrumbs";
 import { ArticleTools } from "@/components/ArticleTools";
 import { editorialReviewDate } from "@/lib/editorial";
 
-export const dynamicParams = false;\n\nexport function generateStaticParams() {
+export const dynamicParams = false;
+
+export function generateStaticParams() {
   return articles.map((article) => ({ slug: article.slug }));
 }
 
