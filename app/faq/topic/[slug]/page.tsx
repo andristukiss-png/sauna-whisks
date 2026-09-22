85d1a68d6dbce96ed15940c112b5a134e52e0ff8
andristukiss-png <andristukiss@gmail.com>
2026-09-23 00:00:52 +0100

    Repair static route newline encoding

--- app/faq/topic/[slug]/page.tsx
+++ app/faq/topic/[slug]/page.tsx
@@ -5,7 +5,9 @@ import { SiteFooter } from "@/components/SiteFooter";
 import { Breadcrumbs } from "@/components/Breadcrumbs";
 import { faqTopics, getFAQTopic } from "@/lib/faqTopics";
 
-export const dynamicParams = false;\n\nexport function generateStaticParams() {
+export const dynamicParams = false;
+
+export function generateStaticParams() {
   return faqTopics.map((topic) => ({ slug: topic.slug }));
 }
 
