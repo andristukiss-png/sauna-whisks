9aa6c5b0b90bd33c4b327f458871bbb5760297c8
andristukiss-png <andristukiss@gmail.com>
2026-09-22 23:55:12 +0100

    Make finite dynamic route static-only

--- app/materials/[slug]/page.tsx
+++ app/materials/[slug]/page.tsx
@@ -6,7 +6,7 @@ import { SiteFooter } from "@/components/SiteFooter";
 import { Breadcrumbs } from "@/components/Breadcrumbs";
 import { getMaterialKnowledge, materialKnowledge } from "@/lib/materialKnowledge";
 
-export function generateStaticParams() {
+export const dynamicParams = false;\n\nexport function generateStaticParams() {
   return materialKnowledge.map((item) => ({ slug: item.slug }));
 }
 
