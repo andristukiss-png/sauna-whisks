fe94bce71b7ad531db20a7163493131136b9be4e
andristukiss-png <andristukiss@gmail.com>
2026-09-22 23:55:33 +0100

    Make finite dynamic route static-only

--- app/journal/topic/[slug]/page.tsx
+++ app/journal/topic/[slug]/page.tsx
@@ -6,7 +6,7 @@ import { SiteFooter } from "@/components/SiteFooter";
 import { Breadcrumbs } from "@/components/Breadcrumbs";
 import { articleTopics, getArticleTopic, getTopicArticles } from "@/lib/articleTopics";
 
-export function generateStaticParams() {
+export const dynamicParams = false;\n\nexport function generateStaticParams() {
   return articleTopics.map((topic) => ({ slug: topic.slug }));
 }
 
