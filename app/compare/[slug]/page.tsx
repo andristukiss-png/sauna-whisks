aa6ea18ed3c710e8bf24830ec4dda8dff59b8eb0
andristukiss-png <andristukiss@gmail.com>
2026-09-23 00:00:45 +0100

    Repair static route newline encoding

--- app/compare/[slug]/page.tsx
+++ app/compare/[slug]/page.tsx
@@ -6,7 +6,9 @@ import { SiteFooter } from "@/components/SiteFooter";
 import { Breadcrumbs } from "@/components/Breadcrumbs";
 import { comparisons, getComparison } from "@/lib/comparisons";
 
-export const dynamicParams = false;\n\nexport function generateStaticParams() {
+export const dynamicParams = false;
+
+export function generateStaticParams() {
   return comparisons.map((item) => ({ slug: item.slug }));
 }
 
