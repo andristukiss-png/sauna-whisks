55980002af3947c3369504b1cd43ed425c65e933
andristukiss-png <andristukiss@gmail.com>
2026-09-22 13:45:37 +0100

    Add Discovery Trio to sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -7,6 +7,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
   const staticPages = [
     "",
     "/shop",
+    "/shop/discovery-trio",
     "/traditions",
     "/journal",
     "/glossary",
