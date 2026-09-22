2c79701dccd0e50daa62863d046ff3b7cc8b5a7f
andristukiss-png <andristukiss@gmail.com>
2026-09-23 00:00:56 +0100

    Repair static route newline encoding

--- app/use-cases/[slug]/page.tsx
+++ app/use-cases/[slug]/page.tsx
@@ -6,7 +6,9 @@ import { SiteFooter } from "@/components/SiteFooter";
 import { Breadcrumbs } from "@/components/Breadcrumbs";
 import { getUseCase, useCases } from "@/lib/useCases";
 
-export const dynamicParams = false;\n\nexport function generateStaticParams(){
+export const dynamicParams = false;
+
+export function generateStaticParams(){
   return useCases.map((item)=>({slug:item.slug}));
 }
 
