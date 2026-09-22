0a14d1b7d50034301120d6c35665aff26d2d5a44
andristukiss-png <andristukiss@gmail.com>
2026-09-22 23:55:17 +0100

    Make finite dynamic route static-only

--- app/trade/[slug]/page.tsx
+++ app/trade/[slug]/page.tsx
@@ -6,7 +6,7 @@ import { Breadcrumbs } from "@/components/Breadcrumbs";
 import { EnquiryForm } from "@/components/EnquiryForm";
 import { getTradeSegment, tradeSegments } from "@/lib/tradeSegments";
 
-export function generateStaticParams() {
+export const dynamicParams = false;\n\nexport function generateStaticParams() {
   return tradeSegments.map((segment) => ({ slug: segment.slug }));
 }
 
