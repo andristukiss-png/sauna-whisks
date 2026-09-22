75fe0dd7a5d0b56ecbc043e538f82edbd41b73ca
andristukiss-png <andristukiss@gmail.com>
2026-09-22 15:46:09 +0100

    Add app/feed.xml/route.ts

---
+++ app/feed.xml/route.ts
@@ -0,0 +1,42 @@
+import { articles } from "@/lib/articles";
+
+function escapeXml(value: string) {
+  return value
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&apos;");
+}
+
+export function GET() {
+  const items = articles
+    .map(
+      (article) => `
+      <item>
+        <title>${escapeXml(article.title)}</title>
+        <link>https://saunawhisks.com/journal/${article.slug}</link>
+        <guid>https://saunawhisks.com/journal/${article.slug}</guid>
+        <description>${escapeXml(article.description)}</description>
+      </item>`
+    )
+    .join("");
+
+  const xml = `<?xml version="1.0" encoding="UTF-8" ?>
+<rss version="2.0">
+  <channel>
+    <title>Sauna Whisks Journal</title>
+    <link>https://saunawhisks.com/journal</link>
+    <description>Guides to sauna whisks, pirts, venik, vihta, materials, care and preparation.</description>
+    <language>en</language>
+    ${items}
+  </channel>
+</rss>`;
+
+  return new Response(xml, {
+    headers: {
+      "Content-Type": "application/rss+xml; charset=utf-8",
+      "Cache-Control": "public, max-age=3600",
+    },
+  });
+}
