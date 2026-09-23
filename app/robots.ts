54463ff1a649f305805a1bbe533c8c174c07ca37
andristukiss-png <andristukiss@gmail.com>
2026-09-23 22:45:10 +0100

    Disallow API prefix consistently in robots policy

--- app/robots.ts
+++ app/robots.ts
@@ -15,7 +15,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: "*",
       allow: "/",
-      disallow: ["/api/"],
+      disallow: ["/api"],
     },
     sitemap: `${site.origin}/sitemap.xml`,
     host: site.origin,
