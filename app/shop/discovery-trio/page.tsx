e10c8f0b19b93623ab858b216377739305482876
andristukiss-png <andristukiss@gmail.com>
2026-09-24 08:44:46 +0100

    Use shared structured data renderer

--- app/shop/discovery-trio/page.tsx
+++ app/shop/discovery-trio/page.tsx
@@ -1,3 +1,4 @@
+import { StructuredData } from "@/components/StructuredData";
 import site from "@/config/site.json";
 import { pageMetadata } from "@/lib/metadata";
 import Link from "next/link";
@@ -29,10 +30,7 @@ export default function DiscoveryTrioPage() {
 
   return (
     <main>
-      <script
-        type="application/ld+json"
-        dangerouslySetInnerHTML={{ __html: JSON.stringify(productSchema).replace(/</g, "\\u003c") }}
-      />
+      <StructuredData data={productSchema} />
       <Header />
       <Breadcrumbs items={[
         { label: "Home", href: "/" },
